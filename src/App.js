fa197ae9a32edf5219c5493c1d6fd0cf45082a22
yuriyfpmislata <yuriy997+clase@gmail.com>
2019-05-03 18:43:53 +0200

    App__main container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.scss';
 import Header from './components/Header/Header';
+import Home from './views/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main className="App__main">
+        <Home />
+      </main>
     </div>
   );
 }
---
+++ src/views/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.scss';
+
+function Home() {
+  return (
+    <div className="Home">
+      <h2>Artists</h2>
+    </div>
+  );
+}
+
+export default Home;
