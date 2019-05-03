b2263f3cc1ace903c7c7f6d7d951a625cffa38ff
yuriyfpmislata <yuriy997+clase@gmail.com>
2019-05-03 18:22:57 +0200

    prepare scss support & cleanup

--- src/App.js
+++ src/App.js
@@ -1,54 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-
-async function getSongs() {
-  const { songs } = await fetch(`${process.env.REACT_APP_ZOOP_API_URL}/songs`).then(res => res.json());
-  return songs;
-}
-
-
-async function getTest() {
-  return await fetch(`${process.env.REACT_APP_ZOOP_API_URL}/graphql`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      query: `
-        {
-          test
-        }
-      `
-    })
-  }).then(res => res.json());
-}
+import React from 'react';
+import './App.scss';
 
 function App() {
-
-  const [songs, setSongs] = useState([]);
-  const [test, setTest] = useState('');
-
-  useEffect(() => {
-    getSongs()
-      .then(setSongs);
-
-    getTest()
-      .then(({ data }) => setTest(data.test));
-  }, []);
-
   return (
     <div className="App">
-      <header className="App-header">
-        <h4>Songs</h4>
-        <ul>
-          {
-            songs.map(song => (
-              <li key={song._id}>{song.name} - {song.artist}</li>
-            ))
-          }
-        </ul>
-        <h4>GraphQL test</h4>
-        <p>{test}</p>
+      <header>
+        <h1>zoop</h1>
       </header>
     </div>
   );
