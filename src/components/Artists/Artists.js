bd6d9c44ad8dcc446ea18f04d970af9a94d5511e
yuriyfpmislata <yuriy997+clase@gmail.com>
2019-05-04 16:42:13 +0200

    Artists: progress bar when fetching

--- src/components/Artists/Artists.js
+++ src/components/Artists/Artists.js
@@ -5,14 +5,13 @@ import ArtistCard from './ArtistCard/ArtistCard';
 function Artists({ artists }) {
   return (
     <div className="Artists">
-      <h2 className="Artists__title title is-4">Artists</h2>
       <div className="Artists__grid">
         {
           artists.length > 0
             ? artists.map(artist => (
               <ArtistCard key={artist._id} artist={artist} />
             ))
-            : 'Fetching artists...'
+            : <progress class="progress is-small is-primary" max="100"></progress>
         }
       </div>
     </div>
