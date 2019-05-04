e491ab52482124e70e90434ec8a6ee83de5a0058
yuriyfpmislata <yuriy997+clase@gmail.com>
2019-05-04 15:51:56 +0200

    style(Header): bg white, brand centered

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,10 +4,10 @@ import './Header.scss';
 function Header() {
   return (
     <header className="Header">
-      <nav className="navbar has-background-primary">
+      <nav className="navbar has-background-white">
         <div className="navbar-brand">
           <div className="navbar-item">
-            <h1 className="title is-2 has-text-white">zoop</h1>
+            <h1 className="title is-2 has-text-primary">zoop</h1>
           </div>
         </div>
       </nav>
