5e9cc5906a7f7162f4dcec41bee2249c4f3c6916
yuriyfpmislata <yuriy997+clase@gmail.com>
2019-05-04 16:28:22 +0200

    add react-router-dom & initial route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import './App.scss';
 import Header from './components/Header/Header';
 import Home from './views/Home/Home';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Artists from './components/Artists/Artists';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <main className="App__main container">
-        <Home />
+        <Router>
+          <Link to="/">Home</Link>
+
+          <Route path="/" exact component={Home} />
+        </Router>
       </main>
     </div>
   );
