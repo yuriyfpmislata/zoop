6fca3fd9684254782df8b9b2629e85b2592098a7
yuriyfpmislata <yuriy997+clase@gmail.com>
2019-05-11 18:43:15 +0200

    ArtistCard: minor fixes

--- src/components/Artists/ArtistCard/ArtistCard.js
+++ src/components/Artists/ArtistCard/ArtistCard.js
@@ -10,7 +10,7 @@ function ArtistCard({ artist, history }) {
           <img src={artist.image} alt="ArtistCard's avatar" />
         </figure>
       </div>
-      <div className="card-content has-text-centered">
+      <div className="ArtistCard__content card-content has-text-centered">
         <h3 className="title is-6">{artist.name}</h3>
       </div>
     </div>
