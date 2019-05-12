01a3f4ac7fdc462fc39cca9374ee4b1c4205b26d
yuriyfpmislata <yuriy997+clase@gmail.com>
2019-05-12 13:37:28 +0200

    Artist: go to Album, genres

--- src/views/Artist/Artist.js
+++ src/views/Artist/Artist.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect, useContext } from 'react';
 import './Artist.scss';
+import { withRouter } from 'react-router-dom';
 
 import * as artistsService from '../../services/artists';
 import PlayerContext from '../../contexts/PlayerContext';
 
-function Artist({ match: route }) {
+function Artist({ match: route, history }) {
   const artistId = route.params.artistId;
 
   const [artist, setArtist] = useState(null);
@@ -30,14 +31,14 @@ function Artist({ match: route }) {
                 <div className="Artist__albums">
                   {
                     artist.albums.map(album => (
-                      <div className="card">
+                      <div className="Artist__album card" onClick={() => history.push(`/artist/${artistId}/album/${album._id}`)}>
                         <div className="card-image">
                           <figure className="image">
                             <img src={album.artwork} alt="Album artwork" />
                           </figure>
                         </div>
                         <div className="Artist__albumName card-content has-text-centered">
-                          <h3 className="title is-6">{album.name}</h3>
+                          <h3 className="title is-6 has-text-primary">{album.name}</h3>
                         </div>
                       </div>
                     ))
@@ -45,13 +46,18 @@ function Artist({ match: route }) {
                 </div>
               </div>
               <section className="Artist__songs section">
-                <h3 className="title is-5">Songs</h3>
+                <h3 className="title is-5">All Songs</h3>
                 <ul>
                   {
                     artist.songs.map(song => (
                       <div className="Artist__song media card" onClick={() => playSong(song._id)}>
                         <div className="media-content">
                           <h5 className="is-size-5 has-text-primary">{song.name}</h5>
+                          <div className="Artist__songGenres">
+                            {
+                              song.genres.map(genre => <span className="tag is-primary">{genre}</span>)
+                            }
+                          </div>
                         </div>
                         <div className="media-right">
                           <span className="is-size-4 has-text-primary">▸</span><span className="Song__playCount has-text-primary">{song.playCount}</span>
@@ -69,4 +75,4 @@ function Artist({ match: route }) {
   )
 }
 
-export default Artist;
+export default withRouter(Artist);
